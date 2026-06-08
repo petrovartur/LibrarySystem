d7c4a9fc659e372197d6f4a1a2f25c0703dc875e
petrovartur <petrovartur989@gmail.com>
2026-06-08 22:20:47 +0500

    Book.java

---
+++ Book.java
@@ -0,0 +1,67 @@
+package model;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private boolean available;
+
+    public Book(int id, String title, String author, int year, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.available = available;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", available=" + available +
+                '}';
+    }
+}
